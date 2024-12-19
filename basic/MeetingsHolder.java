63cb9fa543613294106cad21aed654dce557690c
Bartosz Romanowski <bartosz.romanowski2@gmail.com>
2024-12-19 08:58:49 +0100

    Update MeetingsHolder.java

--- basic/MeetingsHolder.java
+++ basic/MeetingsHolder.java
@@ -1,9 +1,11 @@
 package basic;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -16,11 +18,10 @@ class MeetingsHolder {
     private final Map<Long, Set<Long>> companiesAvailableTimeslots;
     private final Set<Long> usersThatAllowGroupMeetings;
     private final Set<Long> companiesThatAllowGroupMeetings;
+    private final Comparator<Long> usersComparator;
+    private final Set<Pair> lockedPairs = new HashSet<>();
 
-    private Long lockedCompany;
-    private Long lockedTimeslot;
-
-    MeetingsHolder(final RoomsHolder roomsHolder, final TimeslotsHolder timeslotsHolder, final Map<Long, Map<Long, Meeting>> soloMeetings, final Map<Long, Map<Long, Meeting>> groupMeetings, final Map<Long, Set<Long>> usersAvailableTimeslots, final Map<Long, Set<Long>> companiesAvailableTimeslots, final Set<Long> usersThatAllowGroupMeetings, final Set<Long> companiesThatAllowGroupMeetings) {
+    MeetingsHolder(final RoomsHolder roomsHolder, final TimeslotsHolder timeslotsHolder, final Map<Long, Map<Long, Meeting>> soloMeetings, final Map<Long, Map<Long, Meeting>> groupMeetings, final Map<Long, Set<Long>> usersAvailableTimeslots, final Map<Long, Set<Long>> companiesAvailableTimeslots, final Set<Long> usersThatAllowGroupMeetings, final Set<Long> companiesThatAllowGroupMeetings, final Comparator<Long> usersComparator) {
         this.roomsHolder = roomsHolder;
         this.timeslotsHolder = timeslotsHolder;
         this.soloMeetings = soloMeetings;
@@ -29,6 +30,7 @@ class MeetingsHolder {
         this.companiesAvailableTimeslots = companiesAvailableTimeslots;
         this.usersThatAllowGroupMeetings = usersThatAllowGroupMeetings;
         this.companiesThatAllowGroupMeetings = companiesThatAllowGroupMeetings;
+        this.usersComparator = usersComparator;
     }
 
     List<Meeting> meetings() {
@@ -38,9 +40,8 @@ class MeetingsHolder {
                 .toList();
     }
 
-    void match(final long userId, final long companyId) {
-        final var success = addUserToExistingMeeting(userId, companyId) || createMeeting(userId, companyId);
-        if (!success) shiftPreviousMeetingAndRetry(userId, companyId);
+    boolean match(final long userId, final long companyId) {
+        return joinOrCreateMeeting(userId, companyId) || shiftPreviousMeetingAndRetry(userId, companyId);
     }
 
     private boolean joinOrCreateMeeting(final long userId, final long companyId) {
@@ -54,7 +55,7 @@ class MeetingsHolder {
         if (canJoinGroupMeeting) {
             final var timeslots = timeslotsHolder.groupMeetingsTimeslots(userId, companyId);
             for (final var timeslot : timeslots) {
-                if (Objects.equals(timeslot, lockedTimeslot) && Objects.equals(companyId, lockedCompany)) continue;
+                if (lockedPairs.contains(new Pair(companyId, timeslot))) continue;
 
                 if (userAvailableTimeslots.contains(timeslot)) {
                     final var meeting = groupMeetings.get(timeslot).get(companyId);
@@ -77,7 +78,7 @@ class MeetingsHolder {
         if (canJoinGroupMeeting) {
             final var timeslots = timeslotsHolder.groupMeetingsTimeslots(userId, companyId);
             for (final var timeslot : timeslots) {
-                if (Objects.equals(timeslot, lockedTimeslot) && Objects.equals(companyId, lockedCompany)) continue;
+                if (lockedPairs.contains(new Pair(companyId, timeslot))) continue;
 
                 if (userAvailableTimeslots.contains(timeslot) && companyAvailableTimeslots.contains(timeslot)) {
                     final var room = roomsHolder.takeRoomForGroupMeeting(timeslot);
@@ -94,7 +95,7 @@ class MeetingsHolder {
 
         final var timeslots = timeslotsHolder.soloMeetingsTimeslots(userId, companyId);
         for (final var timeslot : timeslots) {
-            if (Objects.equals(timeslot, lockedTimeslot) && Objects.equals(companyId, lockedCompany)) continue;
+            if (lockedPairs.contains(new Pair(companyId, timeslot))) continue;
 
             if (userAvailableTimeslots.contains(timeslot) && companyAvailableTimeslots.contains(timeslot)) {
                 final var room = roomsHolder.takeRoomForSoloMeeting(timeslot);
@@ -111,13 +112,13 @@ class MeetingsHolder {
         return false;
     }
 
-    private void shiftPreviousMeetingAndRetry(final long userId, final long companyId) {
-        final var success = shiftPreviousMeetingAndRetryForGroupMeeting(userId, companyId);
-        if (!success) shiftPreviousMeetingAndRetryForSoloMeeting(userId, companyId);
+    private boolean shiftPreviousMeetingAndRetry(final long userId, final long companyId) {
+        return shiftPreviousMeetingAndRetryForGroupMeeting(userId, companyId)
+               || shiftPreviousMeetingAndRetryForSoloMeeting(userId, companyId);
     }
 
-    private void shiftPreviousMeetingAndRetryForSoloMeeting(final long userId, final long companyId) {
-        shiftPreviousMeetingAndRetry(userId, companyId, timeslotsHolder.soloMeetingsTimeslots(userId, companyId));
+    private boolean shiftPreviousMeetingAndRetryForSoloMeeting(final long userId, final long companyId) {
+        return shiftPreviousMeetingAndRetry(userId, companyId, timeslotsHolder.soloMeetingsTimeslots(userId, companyId));
     }
 
     private boolean shiftPreviousMeetingAndRetryForGroupMeeting(final long userId, final long companyId) {
@@ -134,18 +135,18 @@ class MeetingsHolder {
             if (userAvailableTimeslots.contains(timeslot)) {
                 final var roomExists = roomsHolder.checkIfThereIsAnyFreeRoom(timeslot);
                 if (roomExists) {
-                    final var meeting = findSoloMeeting(timeslot, companyId);
+                    final var meeting = findMeeting(timeslot, companyId);
                     if (meeting != null) {
-                        removeSoloMeeting(meeting);
-                        lockCompanyAndTimeslot(meeting.companyId(), meeting.timeslot());
-                        final var otherUserId = meeting.userIds().getFirst();
-                        final var otherUserSuccess = joinOrCreateMeeting(otherUserId, companyId);
-                        unlockCompanyAndTimeslot();
+                        final var pair = Pair.from(meeting);
+                        lockPair(pair);
+                        removeMeeting(meeting);
+                        final var success = rematchMembers(meeting);
+                        unlockPair(pair);
 
-                        if (otherUserSuccess) {
-                            return joinOrCreateMeeting(userId, companyId);
+                        if (success) {
+                            return match(userId, companyId);
                         } else {
-                            joinOrCreateMeeting(otherUserId, companyId);
+                            meeting.userIds().forEach(otherUserId -> match(otherUserId, companyId));
                         }
                     }
                 }
@@ -154,29 +155,49 @@ class MeetingsHolder {
         return false;
     }
 
-    private void lockCompanyAndTimeslot(final long company, final long timeslot) {
-        lockedCompany = company;
-        lockedTimeslot = timeslot;
+    private boolean rematchMembers(final Meeting meeting) {
+        meeting.userIds().sort(usersComparator);
+        boolean success;
+        for (final Long userId : meeting.userIds()) {
+            success = match(userId, meeting.companyId());
+            if (!success) return false;
+        }
+        return true;
     }
 
-    private void unlockCompanyAndTimeslot() {
-        lockedTimeslot = null;
-        lockedCompany = null;
+    private void lockPair(final Pair pair) {
+        lockedPairs.add(pair);
     }
 
-    private Meeting findSoloMeeting(final long timeslot, final long companyId) {
-        final var meetingsMap = soloMeetings.get(timeslot);
-        return meetingsMap != null ? meetingsMap.get(companyId) : null;
+    private void unlockPair(final Pair pair) {
+        lockedPairs.remove(pair);
     }
 
-    private void removeSoloMeeting(final Meeting meeting) {
+    private Meeting findMeeting(final long timeslot, final long companyId) {
+        final var timeslotSoloMeetings = soloMeetings.get(timeslot);
+        final Meeting soloMeeting;
+        if (timeslotSoloMeetings != null && (soloMeeting = timeslotSoloMeetings.get(companyId)) != null) {
+            return soloMeeting;
+        } else {
+            final var timeslotGroupMeetings = groupMeetings.get(timeslot);
+            return timeslotGroupMeetings != null ? timeslotGroupMeetings.get(companyId) : null;
+        }
+    }
+
+    private void removeMeeting(final Meeting meeting) {
         roomsHolder.add(meeting.timeslot(), meeting.room());
-        soloMeetings.get(meeting.timeslot()).remove(meeting.companyId());
+        (meeting.solo() ? soloMeetings : groupMeetings).get(meeting.timeslot()).remove(meeting.companyId());
         usersAvailableTimeslots.get(meeting.userIds().getFirst()).add(meeting.timeslot());
         companiesAvailableTimeslots.get(meeting.companyId()).add(meeting.timeslot());
     }
 
     private boolean canJoinGroupMeeting(final long userId, final Long companyId) {
         return usersThatAllowGroupMeetings.contains(userId) && companiesThatAllowGroupMeetings.contains(companyId);
+    }
+
+    private record Pair(long companyId, long timeslot) {
+        private static Pair from(final Meeting meeting) {
+            return new Pair(meeting.companyId(), meeting.timeslot());
+        }
     }
 }
